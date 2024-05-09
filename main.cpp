e8bdfe1a06048b5891b42174f9f3e892510eb6d0
VitorHugoCD <vitordelantonia@gmail.com>
2024-05-08 22:19:47 -0300

    feat[minor]: classes finais mapeadas

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
 
+//Classes
 class Medico {
 public:
 
@@ -75,7 +76,6 @@ private:
     int codigoCidade;
     int codigoEspecialidade;
 };
-
 class Cidade {
 public:
 
@@ -119,7 +119,6 @@ private:
     std::string nome;
     std::string uf;
 };
-
 class EspecialidadeMedica {
 public:
 
@@ -156,7 +155,6 @@ private:
     int codigo;
     std::string descricao;
 };
-
 class CID {
 public:
 
@@ -183,6 +181,193 @@ private:
     std::string codigo;
     std::string descricao;
 };
+class Paciente {
+public:
+
+    //Construtor
+    Paciente(std::string cpf, std::string nome, std::string endereco, int codigoCidade) {
+        this->cpf = cpf;
+        this->nome = nome;
+        this->endereco = endereco;
+        this->codigoCidade = codigoCidade;
+    }
+
+    //Getters
+    std::string getCpf() {
+        return this->cpf;
+    }
+    std::string getNome() {
+        return this->nome;
+    }
+    std::string getEndereco() {
+        return this->endereco;
+    }
+    int getCodigoCidade() {
+        return this->codigoCidade;
+    }
+
+    //Setters
+    void setCpf(std::string cpf) {
+        this->cpf = cpf;
+    }
+    void setNome(std::string nome) {
+        this->nome = nome;
+    }
+    void setEndereco(std::string endereco) {
+        this->endereco = endereco;
+    }
+    void setCodigoCidade(int codigoCidade) {
+        this->codigoCidade = codigoCidade;
+    }
+
+private:
+    std::string cpf;
+    std::string nome;
+    std::string endereco;
+    int codigoCidade;
+};
+class Medicamento {
+    //código, descrição, quant_estoque, estoque_minimo, estoque_maximo, preço_unitario
+public:
+
+    //Construtores
+    Medicamento(int ultimoCodigoDeMedicamentoCadastrado) {
+        this->codigo = ultimoCodigoDeMedicamentoCadastrado + 1;
+        this->descricao = "unknown medicament";
+        this->quantidadeEstoque = -1;
+        this->estoqueMinimo = -1;
+        this->estoqueMaximo = -1;
+        this->precoUnitario = -1.0;
+    }
+    Medicamento(int ultimoCodigoDeMedicamentoCadastrado, std::string descricao, int quantidadeEstoque,
+                int estoqueMinimo, int estoqueMaximo, int precoUnitario) {
+        this->codigo = ultimoCodigoDeMedicamentoCadastrado + 1;
+        this->descricao = descricao;
+        this->quantidadeEstoque = quantidadeEstoque;
+        this->estoqueMinimo = estoqueMinimo;
+        this->estoqueMaximo = estoqueMaximo;
+        this->precoUnitario = precoUnitario;
+    }
+
+    //Getters
+    int getCodigo() {
+        return this->codigo;
+    }
+    std::string getDescricao() {
+        return this->descricao;
+    }
+    int getQuantidadeEstoque() {
+        return this->quantidadeEstoque;
+    }
+    int getEstoqueMinimo() {
+        return this->estoqueMinimo;
+    }
+    int getEstoqueMaximo() {
+        return this->estoqueMaximo;
+    }
+    float getPrecoUnitario() {
+        return this->precoUnitario;
+    }
+
+    //Setters
+    void setDescricao(std::string descricao) {
+        this->descricao = descricao;
+    }
+    void setQuantidadeEstoque(int quantidadeEstoque) {
+        this->quantidadeEstoque = quantidadeEstoque;
+    }
+    void setEstoqueMaximo(int estoqueMaximo) {
+        this->estoqueMaximo = estoqueMaximo;
+    }
+    void setEstoqueMinimo(int estoqueMinimo) {
+        this->estoqueMinimo = estoqueMinimo;
+    }
+    void setPrecoUnitario(float precoUnitario) {
+        this->precoUnitario = precoUnitario;
+    }
+
+private:
+    int codigo;
+    std::string descricao;
+    int quantidadeEstoque;
+    int estoqueMinimo;
+    int estoqueMaximo;
+    float precoUnitario;
+};
+class Consulta {
+    // cpf_paciente, cod_medico, data, horario, cod_CID, cod_medicamento, qtde_medicamento
+public:
+
+    //Construtor
+    Consulta(std::string cpf, int codigoMedico, std::string data, std::string horario, std::string codigoCID,
+             int codigoMedicamento, int quantidadeMedicamento) {
+        this->cpfPaciente = cpf;
+        this->codigoMedico = codigoMedico;
+        this->data = data;
+        this->horario = horario;
+        this->codigoDoenca = codigoDoenca;
+        this->codigoMedicamento = codigoMedicamento;
+        this->quantidadeMedicamento = quantidadeMedicamento;
+    }
+
+    //Getters
+    std::string getCpfPaciente() {
+        return this->cpfPaciente;
+    };
+    int getCodigoMedico() {
+        return this->codigoMedico;
+    };
+    std::string getData() {
+        return this->data;
+    };
+    std::string getHorario() {
+        return this->horario;
+    };
+    std::string getCodigoDoenca() {
+        return this->codigoDoenca;
+    };
+    int getCodigoMedicamento() {
+        return this->codigoMedicamento;
+    };
+    int getQuantidadeMedicamento() {
+        return this->quantidadeMedicamento;
+    };
+
+    //Setters
+    void setCpfPaciente(std::string cpfPaciente) {
+        this->cpfPaciente = cpfPaciente;
+    };
+    void setCodigoMedico(int codigoMedico) {
+        this->codigoMedico = codigoMedico;
+    };
+    void setData(std::string data) {
+        this->data = data;
+    };
+    void setHorario(std::string horario) {
+        this->horario = horario;
+    };
+    void setCodigoDoenca(std::string codigoDoenca) {
+        this->codigoDoenca = codigoDoenca;
+    };
+    void setCodigoMedicamento(int codigoMedicamento) {
+        this->codigoMedicamento = codigoMedicamento;
+    };
+    void setQuantidadeMedicamento(int quantidadeMedicamento) {
+        this->quantidadeMedicamento = quantidadeMedicamento;
+    };
+
+
+private:
+    std::string cpfPaciente;
+    int codigoMedico;
+    std::string data;
+    std::string horario;
+    std::string codigoDoenca;
+    int codigoMedicamento;
+    int quantidadeMedicamento;
+
+};
+//Fim-Classes
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
